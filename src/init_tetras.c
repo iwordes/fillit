a8f367fc7451585aafeeacc7e4890c422a2137cd
Ian Wordes <iwordes@student.42.us.org>
2016-11-16 16:18:07 -0800

    Fix issue where files ending in an extra newline would validate

--- src/init_tetras.c
+++ src/init_tetras.c
@@ -17,22 +17,24 @@ t_tetra				*init_tetras(int fd)
 	static t_tetra	tetras[27];
 	char			buffer[21];
 	int				bytes;
+	int				last;
 	int				t;
 
 	t = 0;
+	last = FALSE;
 	while ((bytes = read(fd, buffer, 21)) > 0)
 	{
-		FAILIF(bytes < 20 || (bytes == 21 && buffer[20] != '\n')
+		FAILIF(last == TRUE || bytes < 20 || (bytes == 21 && buffer[20] != '\n')
 			|| buffer[4] != '\n' || buffer[9] != '\n'
 			|| buffer[14] != '\n' || buffer[19] != '\n'
 			|| !validate_buffer(buffer));
 		create_tetra((char (*)[5])buffer, tetras + t, t);
 		align_tetra(tetras + t);
-		t++;
 		if (bytes == 20)
-			break ;
+			last = TRUE;
+		t++;
 	}
-	FAILIF(t == 0 || bytes < 0);
+	FAILIF(t == 0 || bytes < 0 || last == FALSE);
 	tetras[t].letter = 0;
 	return (tetras);
 }
